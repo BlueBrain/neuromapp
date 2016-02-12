cc5ff9c6786c36112780f4ba7ccc0c13bcd5ca68
Kai Langen <kai.langen@epfl.ch>
2016-02-12 17:40:04 +0100

    added omp to the simulation
    Change-Id: Iba4457e228dce66a731c45f0dc00c5011efc88f9

--- neuromapp/spike/mpiexec.cpp
+++ neuromapp/spike/mpiexec.cpp
@@ -1,13 +1,20 @@
 // Include the MPI version 2 C++ bindings:
 #include <mpi.h>
 #include <iostream>
+#include <ctime>
 #include <stdlib.h>
 #include <cassert>
+#include <sys/time.h>
 
 #include "utils/storage/neuromapp_data.h"
 #include "spike/algos.hpp"
 #include "spike/environment.h"
 #include "coreneuron_1.0/queueing/pool.h"
+
+#ifdef _OPENMP
+    #include <omp.h>
+#endif
+
 
 int main(int argc, char* argv[]) {
     assert(argc == 6);
@@ -22,13 +29,22 @@ int main(int argc, char* argv[]) {
     int numIn = atoi(argv[4]);
     int isDistributed = atoi(argv[5]);
 
+#ifdef _OPENMP
+    omp_set_num_threads(8);
+#endif
+
+    struct timeval start, end;
     assert(numIn <= (numOut * (size - 1)));
 
 //    spike::environment env(eventsPer, numOut, numIn, simTime, size, rank);
     queueing::pool<queueing::mutex> env(64, eventsPer, 90, false, true, 4, numOut, numIn, size, rank);
 
+    gettimeofday(&start, NULL);
     run_sim(env, simTime, false);
-
+    gettimeofday(&end, NULL);
+    long long diff_ms = (1000 * (end.tv_sec - start.tv_sec)) + ((end.tv_usec - start.tv_usec) / 1000);
+    if(rank == 0)
+        std::cout<<"run time: "<<diff_ms<<" ms"<<std::endl;
 //    std::cout<<env.received()<<std::endl;
   //  std::cout<<env.all_matching()<<std::endl;
 
