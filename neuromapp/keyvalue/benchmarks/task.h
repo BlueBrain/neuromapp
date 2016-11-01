34a1b1d2ca66cb4ab4851f1eab95936309450a72
Judit Planas <judit.planas@epfl.ch>
2016-11-01 14:23:52 +0100

    [keyvalue] Fixed compilation error.

--- neuromapp/keyvalue/benchmarks/task.h
+++ neuromapp/keyvalue/benchmarks/task.h
@@ -42,7 +42,7 @@ template<keyvalue::selector S>
 class benchmark;
 
 template<keyvalue::selector S>
-keyvalue::statistic task(benchmark<S> const & b){
+keyvalue::statistic task(benchmark<S> & b){
     typedef typename keyvalue::trait_meta<S>::meta_type meta_type; // get the good meta type
     // extract the group of memory and the argument
     keyvalue::group<meta_type> & g = b.get_group_nc(); //get the group
