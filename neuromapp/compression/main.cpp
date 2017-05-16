75e3a504076e4256b5119a5c65b52bce92485422
DevinBayly <baylyd@email.arizona.edu>
2017-05-15 17:33:16 -0700

    add program_options to the compression mapp execute, now able to get return of memory allocated after commandline input

--- neuromapp/compression/main.cpp
+++ neuromapp/compression/main.cpp
@@ -7,53 +7,61 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 #include <boost/program_options.hpp>
 // the includes are relative to the directory above
 #include "compression/allocator.h"
 #include "compression/exception.h"
 #include "compression/block.h"
-// do I need  to use the util error?
+//? do I need  to use the util error?
 
 /* make namespace alias for program options */
+using neuromapp::block;
+using neuromapp::cstandard;
 namespace po = boost::program_options;
+// todo change to variable type, and allocation method
+using std_block = block<int,cstandard>;
 
-//also need to make it possible to only get one of these
-block & gen_block(int n,int m=0)
+//todo decide if pointer is right return value 
+std_block * gen_block(std::vector<int> vec)
 {
-    if (m != 0) {
-        block new_block(n,m);// hopefully constructor dishes out enough heap space for this
-    } else {
+    if (vec.size() == 2) {
+        std_block * new_block = new std_block(vec[0],vec[1]);// hopefully constructor dishes out enough heap space for this
+        return new_block;
+    } else if (vec.size() == 1) {
         // only 1d block
-        block new_block(n);
+        std_block* new_block = new std_block(vec[0]);
+        return new_block;
     }
-    return new_block;
 
 }
 
-//include the boost program options later on
-//first make sure that the compression option can be shown.
+//todo include more boost program_options later on
 int comp_execute(int argc,char *const argv[])
 {
     try {
-        //!make sure to read up on notifiers before setting this in stone
         //make desc
-        po::option_description desc{"Allowed Options"};
+        po::options_description desc{"Allowed options"};
         //add options
         desc.add_options()
-            //?need to figure out how to pass two int values to the  notifier
             //register gen_block with the create user option
-            ("create",po::value<int>()->notifier(&gen_block),"Make a block with these dimensions")
+            ("help","Print out the help screen")
+            ("create",po::value< std::vector<int>>()->multitoken(),"The create option");
         //create variable map
         po::variables_map vm;
         po::store(po::parse_command_line(argc,argv,desc),vm);
         //finish function registration with notify
-        notify(vm);
+        po::notify(vm);
         //loop over reasonable options
-        if(vm.count("create")){
-            vm[create].as<int>();// this should launch the gen_block function
+        if (vm.count("help")) {
+            std::cout << desc << std::endl;
         }
-        
-
+        if(vm.count("create")){
+            std::vector<int> opts;
+            opts = vm["create"].as<std::vector <int>>();// this should launch the gen_block function
+            std_block * created_block = gen_block(opts);
+            std::cout << created_block->memory_allocated() << std::endl;
+        }
     }
     catch (po::error &e) {
         std::cerr << e.what()  << std::endl;
