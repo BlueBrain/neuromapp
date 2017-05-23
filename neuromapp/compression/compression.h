95ee5175189fbacf690c6a1f5d5de4a3d2c6b0ce
DevinBayly <baylyd@email.arizona.edu>
2017-05-22 19:43:40 -0700

    add prototype for the hello_zlib function from zlib nspace

--- neuromapp/compression/compression.h
+++ neuromapp/compression/compression.h
@@ -1,7 +1,9 @@
 #ifndef MAPP_COMPRESSION_EXECUTE_
 #define MAPP_COMPRESSION_EXECUTE_ 
 // todo have another file in the directory that could have a function called by the execute so that I don't have to have the function in the main.cpp
-//void hello_zlib();
+namespace {
+    void hello_zlib();
+}
 //above is taken from the zlib file
 
 int comp_execute(int argc,char *const argv[]);
