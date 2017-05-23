87861cef91adbcce6560085808d72287911a1c5c
DevinBayly <baylyd@email.arizona.edu>
2017-05-22 19:45:35 -0700

    change memvolts reference to be relative to the app build source, modify calls to enter data

--- neuromapp/compression/main.cpp
+++ neuromapp/compression/main.cpp
@@ -5,6 +5,8 @@
 * used for the neuromapp when compression is typed out as option. 
 */
 
+#include <sys/stat.h>
+#include <stdexcept>
 #include <iterator>
 #include <algorithm>
 #include <fstream>
@@ -13,12 +15,11 @@
 #include <vector>
 #include <boost/program_options.hpp>
 // the includes are relative to the directory above
-#include "compression.h"
+#include "compression/compression.h"
+#include "compression/zlib_fns.h"
 #include "compression/allocator.h"
 #include "compression/exception.h"
 #include "compression/block.h"
-// include the zlib header.
-#include "/usr/include/zlib.h"
 
 /* make namespace alias for program options */
 using neuromapp::block;
@@ -47,7 +48,6 @@ int comp_execute(int argc,char *const argv[])
 {
     //prototype for the hello_zlib
     //
-    void hello_zlib();
     void enter_data(std_block &,const std::string &);
 
     //now the program options section
@@ -67,7 +67,7 @@ int comp_execute(int argc,char *const argv[])
         po::notify(vm);
         //loop over reasonable options
         if (vm.count("demo")) {
-            hello_zlib();
+            zlib::hello_zlib();
         }
         if (vm.count("help")) {
             std::cout << desc << std::endl;
@@ -78,7 +78,7 @@ int comp_execute(int argc,char *const argv[])
             dims = vm["create"].as<std::vector <int>>();// this should launch the gen_block function
             std_block * created_block = gen_block(dims);
             //try entering data from the memvolts file of randomly generated membrane values, use the *created_block to prevent copy argument passing
-            enter_data(*created_block,"/home/lil/Documents/BlueBrainProj_study-abroad/gsoc/memvolts.csv");
+            created_block->enter_data("../memvolts.csv");
             // and then try  to print out the block with the print template funciton
             created_block->print();
         }
@@ -90,41 +90,3 @@ int comp_execute(int argc,char *const argv[])
     return 0;
 }
 
-void hello_zlib() {
-    char a[] = "hello world";
-    char b[50];
-    z_stream def_stream;// this means the struct that will be used for deflation
-
-    //and now setting the initial struct variables
-    def_stream.zalloc = Z_NULL;
-    def_stream.zfree = Z_NULL;
-    def_stream.opaque = Z_NULL;
-    def_stream.avail_in = (uInt)strlen(a) + 1;// makes space for the null char?
-    def_stream.next_in = (Bytef*) a;// this means the first byte is from array a
-    def_stream.next_out = (Bytef*)b;
-    deflateInit(&def_stream,Z_DEFAULT_COMPRESSION);// initializes the compression of array a
-    deflate(&def_stream,Z_FINISH);// begins compression
-    deflateEnd(&def_stream);// frees the structure used for compression
-
-    // method for getting size of compressed array
-    printf("Deflated size is :%lu\n",(char*)def_stream.next_out -b );// think about this line
-    /* vector<char> file_chars(start,end); */
-    /* for_each(file_chars.begin(),file_chars.end(),[](char letter){cout << ((letter == 'e') ? letter : 'X');}); */
-}
-// this is the tool for adding entries to our block
-void enter_data(std_block & empty_block,const std::string & fname)
-{
-    //could use either the iterator, or the indexing, dunno which is better
-    std::ifstream in_file(fname);
-    std::cout << fname << std::endl;
-    auto block_it = empty_block.begin();
-    std::string str_temp {""};
-    while(in_file >> str_temp && block_it != empty_block.end()){
-        int int_temp {stoi(str_temp)};
-        *block_it = int_temp;
-        block_it++;
-    }
-    // close the file
-    in_file.close();
-
-}
