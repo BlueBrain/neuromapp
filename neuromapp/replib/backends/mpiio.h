ebd1665ff0606b6ff30f6bd9f073ae2efc47b9ae
Judit Planas <judit.planas@epfl.ch>
2017-06-02 10:11:40 +0200

    [replib] Refactored code to accept other backends (in addition to MPI I/O). Similar style as iobench mini-app.

---
+++ neuromapp/replib/backends/mpiio.h
@@ -0,0 +1,188 @@
+/*
+ * Neuromapp - benchmark.h, Copyright (c), 2015,
+ * Judit Planas - Swiss Federal Institute of technology in Lausanne,
+ * judit.planas@epfl.ch,
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+#ifndef MAPP_RL_MPIIO_H
+#define MAPP_RL_MPIIO_H
+
+//#include <mpi.h>
+//#include <unistd.h>
+#include <string.h>
+#include <cstring>
+
+#include "replib/backends/basic.h"
+#include "replib/utils/config.h"
+#include "replib/utils/fileview.h"
+#include "replib/mpiio_dist/rnd1b.h"
+#include "replib/mpiio_dist/file1b.h"
+#include "replib/mpiio_dist/fileNb.h"
+
+#include "utils/mpi/controler.h"
+#include "utils/mpi/timer.h"
+//#include "utils/mpi/error.h"
+
+namespace replib {
+
+class MPIIOWriter : public replib::Writer {
+
+    private:
+        /* Fileview of the file, it is private, so no need for getters/setters */
+        replib::fileview * f_;
+
+        /* The following members are private and exclusive of MPI I/O,
+         * so there is no need to implement getters or setters
+         */
+        MPI_Info info_;
+        MPI_File fh_;
+
+
+    public:
+
+        MPIIOWriter();
+        ~MPIIOWriter();
+
+        void init(replib::config &c);
+        void open(const char * path);
+        void open(mapp::timer &t_io, const std::string &path);
+        void write(float * buffer, size_t count);
+        void write(mapp::timer &t_io, float * buffer, size_t count);
+        void close();
+        void close(mapp::timer &t_io);
+        unsigned int total_bytes();
+
+};
+/** \fun MPIIOWriter()
+    \brief create the object, no special actions required for MPI I/O
+ */
+MPIIOWriter::MPIIOWriter() : f_(NULL) {}
+
+
+/** \fun ~MPIIOWriter()
+    \brief Class destructor */
+MPIIOWriter::~MPIIOWriter() {
+    delete(f_);
+}
+
+
+/** \fun init(const replib::config &c)
+    \brief initialize the object using the given configuration */
+void MPIIOWriter::init(replib::config &c) {
+    if (c.write() == "file1b") {
+        f_ = file1b(c);
+    } else if (c.write() == "fileNb") {
+        f_ = fileNb(c);
+    } else { // "rnd1b"
+        f_ = rnd1b(c);
+    }
+
+    // Use MPI_Info to disable collective buffers if using IME backend
+    MPI_Info_create(&info_);
+    std::string ime("ime:/");
+    if (c.output_report().compare(0, ime.length(), ime) == 0) {
+        MPI_Info_set (info_, "romio_ds_write", "disable");
+        MPI_Info_set (info_, "romio_cb_write", "disable");
+    }
+}
+
+
+/** \fun open(const char * path)
+    \brief Open the file with MPI I/O and set the fileview.
+           Inline version to be as fast as possible */
+inline void MPIIOWriter::open(const char * report) {
+    //Open the file
+    int error = MPI_File_open(MPI_COMM_WORLD, report, MPI_MODE_WRONLY | MPI_MODE_CREATE, info_, &fh_);
+    if (error != MPI_SUCCESS) {
+        std::cout << "[" << mapp::controler::getInstance().rank() << "] Error opening file: " << report << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 911);
+    }
+
+    f_->set_fileview(&fh_);
+}
+
+/** \fun open(mapp::timer &t_io, const std::string & path)
+    \brief Open the file with MPI I/O and set the fileview.
+           Timer will be used to account the time of opening the file and setting the fileview */
+void MPIIOWriter::open(mapp::timer &t_io, const std::string & path) {
+    // Get the file path
+    char * report = strdup(path.c_str());
+
+    //Open the file
+    t_io.tic();
+    open(report);
+    t_io.toc();
+}
+
+
+/** \fun write(float * buffer, size_t count)
+    \brief Write to file. Inline version to be as fast as possible */
+inline void MPIIOWriter::write(float * buffer, size_t count) {
+    MPI_Status status;
+    int error = MPI_File_write_all(fh_, buffer, count, MPI_FLOAT, &status);
+
+    if (error != MPI_SUCCESS) {
+        std::cout << "[" << mapp::controler::getInstance().rank() << "] Error writing file" << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 915);
+    }
+}
+
+/** \fun write(mapp::timer &t_io, float * buffer, size_t count)
+    \brief Write to file. Since we need to measure the writing time, this function receives
+           the timer that should be used to surround the write call */
+void MPIIOWriter::write(mapp::timer &t_io, float * buffer, size_t count) {
+    MPI_Status status;
+    // t_io only counts time spent in MPI_File_write_all to compute I/O statistics
+    t_io.tic();
+    int error = MPI_File_write_all(fh_, buffer, count, MPI_FLOAT, &status);
+    t_io.toc();
+
+    if (error != MPI_SUCCESS) {
+        std::cout << "[" << mapp::controler::getInstance().rank() << "] Error writing file" << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 915);
+    }
+}
+
+/** \fun close()
+    \brief Close the file. Inline version to be as fast as possible */
+inline void MPIIOWriter::close() {
+    int error = MPI_File_close(&fh_);
+    if (error != MPI_SUCCESS) {
+        std::cout << "[" << mapp::controler::getInstance().rank() << "] Error closing file" << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 912);
+    }
+}
+
+/** \fun close(mapp::timer &t_io)
+    \brief Close the file.
+           Timer will be used to account the time of closing the file */
+void MPIIOWriter::close(mapp::timer &t_io) {
+    t_io.tic();
+    close();
+    t_io.toc();
+}
+
+/** \fun total_bytes()
+    \brief Return the total amount of data (in bytes) written at each reporting step */
+unsigned int MPIIOWriter::total_bytes() {
+    return f_->total_bytes();
+}
+
+} // end namespace
+
+#endif // MAPP_RL_MPIIO_H
