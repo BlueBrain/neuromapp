f4e621dd626cf20b04b800be0fe24d881c3e4acc
DevinBayly <baylyd@email.arizona.edu>
2017-06-05 18:26:33 -0700

    implement compare policy for tim, and generate a fallback non compression policy for block

--- neuromapp/compression/allocator.h
+++ neuromapp/compression/allocator.h
@@ -5,83 +5,103 @@
 #include <stdlib.h> // POSIX, size_t are inside
 #include <cstring>  // memset
 
+
 #include "exception.h"
+
 
 namespace neuromapp {
 
-class cstandard {
-  public:
-    typedef std::size_t size_type;
+    class cstandard {
+        public:
+            typedef std::size_t size_type;
 
-    //note that the lengine entries used to be neuromap - w/o extra p
-    //todo remove the mention of lengine anywhere
-    /** The allocate function used in the policy */
-    void *allocate_policy(size_type size) {
-        if ((size & 0x8000000000000000)) // size is uint64_t so I check MSB if a negative number is the arg
-            throw cpu_memory_allocation("negative size, cpu cstandard policy", lengine_error::NEUROMAPP_BAD_ARG);
-        else if (size == 0)
-            throw cpu_memory_allocation("null size, cpu cstandard policy", lengine_error::NEUROMAPP_BAD_ARG);
+            //note that the lengine entries used to be neuromap - w/o extra p
+            //todo remove the mention of lengine anywhere
+            /** The allocate function used in the policy */
+            void *allocate_policy(size_type size) {
+                if ((size & 0x8000000000000000)) // size is uint64_t so I check MSB if a negative number is the arg
+                    throw cpu_memory_allocation("negative size, cpu cstandard policy", lengine_error::NEUROMAPP_BAD_ARG);
+                else if (size == 0)
+                    throw cpu_memory_allocation("null size, cpu cstandard policy", lengine_error::NEUROMAPP_BAD_ARG);
 
-        void *ptr = std::malloc(size);
-        std::memset(ptr, 0, size);
-        /* log.insert(size, "cstandard"); */
-        return ptr;
-    }
+                void *ptr = std::malloc(size);
+                std::memset(ptr, 0, size);
+                /* log.insert(size, "cstandard"); */
+                return ptr;
+            }
 
-    /** The deallocate function used in the policy */
-    void deallocate_policy(void *ptr) { std::free(ptr); }
+            /** The deallocate function used in the policy */
+            void deallocate_policy(void *ptr) { std::free(ptr); }
 
-    /** The copy function used in the policy */
-    void copy_policy(void *ptr_destination, void *ptr_source, std::size_t size) {
-        std::memcpy(ptr_destination, ptr_source, size);
-    }
+            /** The copy function used in the policy */
+            void copy_policy(void *ptr_destination, void *ptr_source, std::size_t size) {
+                std::memcpy(ptr_destination, ptr_source, size);
+            }
 
-    constexpr static char policy_name[] = "cstandard";
-};
+            bool compare_policy(const void * rhs,const void * lhs) {
+                // if( != other.num_rows() || 
+                //         this->cols_ != other.num_cols()) {
+                //     std::cerr << "block comparison failed: size was off" << std::endl;
+                //     return false;
+                // }
+                // int rc =std::memcmp(this->data(),other.data(),other.memory_allocated());
+                // if (rc == 0) {
+                //     return true;// typical char* comparison rules
+                // } else {
+                //     std::cerr << "block comparison failed: memcmp of blocks failed" << std::endl;
+                //     return false;
+                // }
+            }
 
-class align {
-  public:
-    typedef std::size_t size_type;
-    const static size_type boundary = 32; // always multiple of 32 byte
+            constexpr static char policy_name[] = "cstandard";
+    };
 
-    /** The allocate function used in the policy */
-    void *allocate_policy(size_type size) {
-        if ((size & 0x8000000000000000)) // size is uint64_t so I check MSB if a negative number is the arg
-            throw cpu_memory_allocation("negative size, cpu align policy", lengine_error::NEUROMAPP_BAD_ARG);
-        else if (size == 0)
-            throw cpu_memory_allocation("null size, cpu align policy", lengine_error::NEUROMAPP_BAD_ARG);
+    class align {
+        public:
+            typedef std::size_t size_type;
+            const static size_type boundary = 32; // always multiple of 32 byte
 
-        // what is getting figured out here?
-        void *ptr = NULL;
-        int rc = posix_memalign(&ptr, boundary, size); // 16,32,64 etc ...
+            /** The allocate function used in the policy */
+            void *allocate_policy(size_type size) {
+                if ((size & 0x8000000000000000)) // size is uint64_t so I check MSB if a negative number is the arg
+                    throw cpu_memory_allocation("negative size, cpu align policy", lengine_error::NEUROMAPP_BAD_ARG);
+                else if (size == 0)
+                    throw cpu_memory_allocation("null size, cpu align policy", lengine_error::NEUROMAPP_BAD_ARG);
 
-        if (rc != 0)
-            throw cpu_memory_allocation("posix allocation fails, cpu align policy", lengine_error::NEUROMAPP_UNKNOWN_ERROR);
+                // what is getting figured out here?
+                void *ptr = NULL;
+                int rc = posix_memalign(&ptr, boundary, size); // 16,32,64 etc ...
 
-        std::memset(ptr, 0, size);
-        /* log.insert(size, "align"); */
+                if (rc != 0)
+                    throw cpu_memory_allocation("posix allocation fails, cpu align policy", lengine_error::NEUROMAPP_UNKNOWN_ERROR);
 
-        return ptr;
-    }
+                std::memset(ptr, 0, size);
+                /* log.insert(size, "align"); */
 
-    /** The deallocate function used in the policy */
-    void deallocate_policy(void *ptr) { std::free(ptr); }
+                return ptr;
+            }
 
-    /** to have each raw align, we may need to add some paddint at the end */
-    inline static size_type resize_policy(size_type size, size_type sizeof_T) {
-        if (sizeof_T > boundary)
-            throw cpu_memory_allocation("wierd, sizeof(type) larger than boundary", lengine_error::NEUROMAPP_BAD_USAGE);
-        size_type element_per_register = boundary / sizeof_T;
-        return (size / element_per_register + 1) * element_per_register;
-    }
+            /** The deallocate function used in the policy */
+            void deallocate_policy(void *ptr) { std::free(ptr); }
 
-    /** The copy function used in the policy */
-    void copy_policy(void *ptr_destination, void *ptr_source, std::size_t size) {
-        std::memcpy(ptr_destination, ptr_source, size);
-    }
+            /** to have each raw align, we may need to add some paddint at the end */
+            inline static size_type resize_policy(size_type size, size_type sizeof_T) {
+                if (sizeof_T > boundary)
+                    throw cpu_memory_allocation("wierd, sizeof(type) larger than boundary", lengine_error::NEUROMAPP_BAD_USAGE);
+                size_type element_per_register = boundary / sizeof_T;
+                return (size / element_per_register + 1) * element_per_register;
+            }
 
-    constexpr static char policy_name[] = "align";
-};
+            /** The copy function used in the policy */
+            void copy_policy(void *ptr_destination, void *ptr_source, std::size_t size) {
+                std::memcpy(ptr_destination, ptr_source, size);
+            }
+
+            constexpr static char policy_name[] = "align";
+            bool compare_policy(const void * lhs,const void *rhs) {
+                //TODO stub
+            }
+    };
 
 } // namespace neuromapp
 
