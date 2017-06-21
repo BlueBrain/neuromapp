19b227552ff39b274c541dbdda4b8217d4594d3a
fouriaux <jeremy.fouriaux@epfl.ch>
2017-06-21 16:54:59 +0200

    add predefined distributions and fix scripts

--- neuromapp/replib/utils/generate_set.py
+++ neuromapp/replib/utils/generate_set.py
@@ -4,11 +4,14 @@ nb_procs      = [16, 32, 128, 256, 512]
 size_per_rank = [4000, 8000, 16000, 4000000, 8000000]
 nb_blocks     = [1,8,32,64]
 
-generators    = ["./uniform_generator.py {ranks} {total_size} --nb_blocks {nb_blocks} > distributions/{rank}_{size}_{nb_blocks}.distrib".
-    format(ranks=ranks, total_size=ranks*size, size=size, nb_blocks=blocks)
+generators    = [("python uniform_generator.py {ranks} {total_size} --nb_blocks {nb_blocks}".
+    format(ranks=ranks, total_size=ranks*size, size=size, nb_blocks=blocks),
+    "distributions/{ranks}_{size}_{nb_blocks}.distrib".format(ranks=ranks, size=size, nb_blocks=blocks))
     for ranks in nb_procs
     for size in size_per_rank
     for blocks in nb_blocks]
 
-for gen in generators :
-  subprocess.exec (gen)
+for gen, filename in generators :
+  print (gen)
+  with open (filename, 'w') as f:
+    result = subprocess.call (gen.split(), stdout=f)
