57b3389f15f1c9a77c376e583dfccb57cfceb4bb
DevinBayly <baylyd@email.arizona.edu>
2017-07-05 17:10:16 -0700

    order of elements in union needs changing

--- neuromapp/compression/bit_shifting.h
+++ neuromapp/compression/bit_shifting.h
@@ -14,9 +14,9 @@ namespace neuromapp {
     typedef union {
         float num;
         struct {
-            unsigned int sign:1;
-            unsigned int exponent:8;
             unsigned int mantissa:23;
+            unsigned int exponent:8;
+            unsigned int sign:1;
         } parts;
     } type_parts;
 
@@ -24,23 +24,30 @@ namespace neuromapp {
     /*function for taking in an array of floats ( TODO make generic) 
      * and creating an unsigned int array that represents its parts
      */
-    void convert_to_parts(pointer row_ptr,block<unsigned int,cstandard> & parts_block,size_type row_num){
+    void convert_to_parts(pointer row_ptr,block<unsigned int,cstandard> & split_block,size_type row_num){
         //TODO make sure to free after use!
-        size_type count = parts_block.dim0()/3;
+        size_type count = split_block.dim0()/3;
         for (size_type i = 0 ; i < count ; i++) {
             type_parts conv_float;
             conv_float.num = *row_ptr++;
-            parts_block(i,row_num) = conv_float.parts.sign;
-            parts_block(i+count,row_num) = conv_float.parts.exponent;
-            parts_block(i+count*2,row_num) = conv_float.parts.mantissa;
+            split_block(i,row_num) = conv_float.parts.sign;
+            split_block(i+count,row_num) = conv_float.parts.exponent;
+            split_block(i+count*2,row_num) = conv_float.parts.mantissa;
         }
     }
 
-    void populate_block_by_parts( block<unsigned int,cstandard> & parts_block,block<value_type,cstandard>& float_block) {
-        size_type row_limit = float_block.num_rows();
+    void populate_block_by_parts( block<unsigned int,cstandard> & split_block,block<value_type,cstandard>& unsplit_block) {
+        size_type row_limit = unsplit_block.num_rows();
         for (size_type row_num = 0; row_num < row_limit;row_num++) {
-            convert_to_parts(&float_block(0,row_num),parts_block,row_num);
+            convert_to_parts(&unsplit_block(0,row_num),split_block,row_num);
         }
+    }
+
+    block<unsigned int, cstandard> generate_split_block(block<value_type,cstandard> & unsplit_block) {
+        size_type rows_ = unsplit_block.num_rows(),cols_=unsplit_block.dim0();
+        block<unsigned int,cstandard> split_block(cols_,rows_);
+        populate_block_by_parts(split_block,unsplit_block);
+        return split_block;
     }
 }
 
