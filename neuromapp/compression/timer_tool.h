dba90be4877960a0f42d52b534f0c8386fdce3f2
DevinBayly <baylyd@email.arizona.edu>
2017-07-07 18:05:04 -0700

    add exception to timer class

--- neuromapp/compression/timer_tool.h
+++ neuromapp/compression/timer_tool.h
@@ -1,16 +1,26 @@
 #include <chrono>
+#include <string>
 #include <iostream>
 using namespace std;
 namespace neuromapp {
     class Timer {
         private:
             chrono::time_point<chrono::system_clock> start,end;
+            chrono::duration<double,std::milli> duration = -1;
         public:
             void start(string & message);
             void start();
             void duration(string & message);
             void duration();
-    }
+        class Exception {
+            private:
+                string _message;
+            public:
+                Exception (string & creation_msg ) : _message{creation_message} {}
+                ~Exception () {};
+
+        };
+    };
 
     void Timer::start(string & message) {
         std::cout << message << std::endl;
@@ -21,15 +31,19 @@ namespace neuromapp {
         start = chrono::system_clock::now();
     }
 
-    void Timer::end(string & message) {
-        std::cout << message << std::endl;
-        Timer::end();
+    void Timer::duration() {
+        //check to see if duration has been set
+        if ( duration == -1) throw (Exception("Duration not initialized, missing end()");
+        return duration;
+    }
+178
+    void Timer::print(string & message) {
+        std::cout << message << " "<< "time: " << duration.count()<< "(ms)" << std::endl;
     }
 
     void Timer::end() {
         end = chrono::system_clock::now();
-        chrono::duration<double,std::milli> duration = end-start;
-        std::cout << "timed: " << duration.count()<< "(ms)" << std::endl;
+        duration = end-start;
     }
 }
 
