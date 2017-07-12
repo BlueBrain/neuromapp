bbd7a5b4d48e8463cc1271639c91a04a0e1a555b
DevinBayly <baylyd@email.arizona.edu>
2017-07-12 14:30:53 -0700

    remove non necessary hello_zlib file

--- neuromapp/compression/compression.h
+++ neuromapp/compression/compression.h
@@ -1,10 +1,5 @@
 #ifndef MAPP_COMPRESSION_EXECUTE_
 #define MAPP_COMPRESSION_EXECUTE_ 
-// todo have another file in the directory that could have a function called by the execute so that I don't have to have the function in the main.cpp
-namespace {
-    void hello_zlib();
-}
-//above is taken from the zlib file
 
 int comp_execute(int argc,char *const argv[]);
 #endif /* ifndef MAPP_COMPRESSION_EXECUTE_ */
