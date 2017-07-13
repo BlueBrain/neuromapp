bff446aba65a9c68f17322dbfefee05ce0a4194f
DevinBayly <baylyd@email.arizona.edu>
2017-07-12 18:34:14 -0700

    add in tim's double specs for the union conversion, make memfuncs generic

--- neuromapp/compression/bit_shifting.h
+++ neuromapp/compression/bit_shifting.h
@@ -7,24 +7,48 @@
 using namespace std;
 using neuromapp::block;
 typedef size_t size_type;
-typedef float value_type;
-typedef float* pointer;
 
 namespace neuromapp {
+    /*base trait structs */
+    template <class T>
+    struct trait_mantissa{};
+    template <class T>
+    struct trait_exponent{};
+
+    /*specializtions for the type converted from */
+    template <>
+        struct trait_mantissa<float> {
+            const static int value = 23;
+        };
+    template <>
+        struct trait_exponent<float> {
+            const static int value = 8;
+        };
+    template <>
+        struct trait_mantissa<double> {
+            const static int value = 52;
+        };
+    template <>
+        struct trait_exponent<double> {
+            const static int value = 11;
+        };
+
+    template<class T>
     typedef union {
-        float num;
+        T num;
         struct {
-            unsigned int mantissa:23;
-            unsigned int exponent:8;
+            unsigned int mantissa = trait_mantissa<T>::value;
+            unsigned int exponent = trait_exponent<T>::value;
             unsigned int sign:1;
         } parts;
     } conversion_parts;
 
 
+    template <value_type,allocator_type>
     /*function for taking in an array of floats ( TODO make generic) 
      * and creating an unsigned int array that represents its parts
      */
-    void convert_to_parts(pointer row_ptr,block<unsigned int,cstandard> & split_block,size_type row_num){
+    void convert_to_parts(pointer row_ptr,block<unsigned int,allocator_type> & split_block,size_type row_num){
         size_type count = split_block.dim0()/3;
         for (size_type i = 0 ; i < count ; i++) {
             conversion_parts conv_float;
@@ -35,10 +59,11 @@ namespace neuromapp {
         }
     }
 
+    template <value_type,allocator_type>
     /*reciprocal function to teh one above, take values from the split block, and convert them back to their floating point representation
      * and store them back in the unsplit block row pointer
      */
-    void convert_from_parts(pointer row_ptr,block<unsigned int,cstandard> & split_block,size_type row_num){
+    void convert_from_parts(pointer row_ptr,block<unsigned int,allocator_type> & split_block,size_type row_num){
         size_type count = split_block.dim0()/3;
         for (size_type i = 0 ; i < count ; i++) {
             conversion_parts conv_float;
@@ -49,14 +74,16 @@ namespace neuromapp {
         }
     }
 
-    void populate_split_block( block<unsigned int,cstandard> & split_block,block<value_type,cstandard>& unsplit_block) {
+    template <value_type,allocator_type>
+    void populate_split_block( block<unsigned int,allocator_type> & split_block,block<value_type,allocator_type>& unsplit_block) {
         size_type row_limit = unsplit_block.num_rows();
         for (size_type row_num = 0; row_num < row_limit;row_num++) {
             convert_to_parts(&unsplit_block(0,row_num),split_block,row_num);
         }
     }
 
-    void populate_unsplit_block( block<unsigned int,cstandard> & split_block,block<value_type,cstandard>& unsplit_block) {
+    template <value_type,allocator_type>
+    void populate_unsplit_block( block<unsigned int,allocator_type> & split_block,block<value_type,allocator_type>& unsplit_block) {
         size_type row_limit = unsplit_block.num_rows();
         for (size_type row_num = 0; row_num < row_limit;row_num++) {
             convert_from_parts(&unsplit_block(0,row_num),split_block,row_num);
@@ -64,16 +91,18 @@ namespace neuromapp {
     }
 
 
-    block<unsigned int, cstandard> generate_split_block(block<value_type,cstandard> & unsplit_block) {
+    template <value_type,allocator_type>
+    block<unsigned int, allocator_type> generate_split_block(block<value_type,allocator_type> & unsplit_block) {
         size_type rows_ = unsplit_block.num_rows(),cols_=unsplit_block.dim0();
-        block<unsigned int,cstandard> split_block(cols_,rows_);
+        block<unsigned int,allocator_type> split_block(cols_,rows_);
         populate_split_block(split_block,unsplit_block);
         return split_block;
     }
     
-    block<value_type,cstandard> generate_unsplit_block(block<unsigned int,cstandard> & split_block) {
+    template <value_type,allocator_type>
+    block<value_type,allocator_type> generate_unsplit_block(block<unsigned int,allocator_type> & split_block) {
         size_type rows_ = split_block.num_rows(),cols_=split_block.dim0();
-        block<value_type,cstandard> unsplit_block(cols_/3,rows_);
+        block<value_type,allocator_type> unsplit_block(cols_/3,rows_);
         populate_unsplit_block(split_block,unsplit_block);
         return unsplit_block;
     }
