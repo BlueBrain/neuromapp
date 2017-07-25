a6fef0c3fc289dcb49cb8a872ba79007167d276c
DevinBayly <baylyd@email.arizona.edu>
2017-07-25 15:35:58 -0700

    add a trivial level 1 compute. Make parameter exp smaller

--- neuromapp/compression/kernel_measurements.cpp
+++ neuromapp/compression/kernel_measurements.cpp
@@ -37,17 +37,35 @@ typedef size_t size_type;
 //TODO add filepath
 #define FNAME "trans_data/values_8_a8780trans_bulk.csv"
 #define ARRAY_SIZE 100 
-enum compress_option {COMPRESS, UNCOMPRESS};
 
 /* Section for the computation functions */
-
 template <typename allocator_type>
 struct level1_compute {
-    double * ptr,double coef,double step,size_type cols,double * end;
+    double coef,* ptr,* end;
     public:
-    level1_compute(double * ptr_arg,double coef_arg,double step_arg,size_type cols_arg,double * end_arg) :  ptr{ptr_arg}, coef{coef_arg}, step{step_arg}, cols{cols_arg}, end {end_arg} {}
-    void operator (ostream & os) () {
+    level1_compute(double * ptr_arg,double coef_arg,double * end_arg) 
+        :coef {coef},ptr{ptr_arg}, end{end_arg} {}
+    void operator () (ostream & os) {
         os << " running lvl1 compute ";
+        while ( ptr != end) {
+            double result = *ptr +coef;
+            ptr++;
+        }
+    }
+};
+
+
+
+
+template <typename allocator_type>
+struct level2_compute {
+    double * ptr, coef, step, * end;
+    size_type cols;
+    public:
+    level2_compute(double * ptr_arg,double coef_arg,double step_arg,size_type cols_arg,double * end_arg) 
+        :  ptr{ptr_arg}, coef{coef_arg}, step{step_arg}, cols{cols_arg}, end {end_arg} {}
+    void operator () (ostream & os) {
+        os << " running lvl2 compute ";
         while (ptr != end) {
             /* now run the computation function on each element */
             double U = *ptr,tau_fac = *ptr + 3*cols;
@@ -57,18 +75,19 @@ struct level1_compute {
     }
 };
 
+
+
 template <typename allocator_type>
 struct level3_compute {
-    double y_initial, double t_initial,double step,double t_limit;
+    double y_initial,  t_initial, step, t_limit;
     public:
     level3_compute (double y_initial_arg, double t_initial_arg,double step_arg,double t_limit_arg) :
         y_initial {y_initial_arg},  t_initial {t_initial_arg}, step {step_arg}, t_limit {t_limit_arg} {}
-
     /* this is the function that you should edit if you want a different solution */
     double differential (double y,double t) {
         return pow(y,2) + t*30;// totally arbitrary for the moment
     }
-    void operator () (){
+    void operator () (ostream & os){
         os << " running lvl3 compute ";
         /* treat the initials as initials for each step, so initial to start, and each step */
         double y_next;
@@ -79,18 +98,6 @@ struct level3_compute {
     }
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
 /* Section for sampling strategies */
 
 template< typename allocator_type >
@@ -100,9 +107,6 @@ void init_array (block<double,allocator_type> * block_array) {
         blk_file.open(FNAME);
         block<double,allocator_type> b1;
         blk_file >> b1;
-#ifdef COMPRESS
-        b1.compress();
-#endif
         block_array[i] = b1;
         blk_file.close();
         blk_file.clear();
@@ -137,17 +141,20 @@ class Block_selector {
 
 /* section for the these things combined */
 template <typename allocator_type,typename fun_ob>
-void kernel_measure(fun_ob & f,ostream & os,compress_option comp_option,block<double,allocator_type> & blk) {
-    /*create and start the timer */
+void kernel_measure(fun_ob & f,ostream & os,block<double,allocator_type> & blk) {
+    /*create and start the timer,compress run first */
     Timer time_it; time_it.start();
-    if(comp_option == COMPRESS) {
-        os << " compressing ";
-        blk.compress();
-    }
-    if(comp_option == COMPRESS) {
-        os << " uncompressing ";
-        blk.uncompress();
-    }
+    os << " compression: " ;
+    blk.uncompress();
+    f(os);
+    blk.compress();
+    time_it.end();
+    os << " duration " << time_it.duration() ;
+    blk.uncompress();
+    /* now the non-compress run */
+    os << " non-compress: " ;
+    time_it.start();
+    f(os);
     time_it.end();
     os << " duration " << time_it.duration() << std::endl;
 }
@@ -155,8 +162,7 @@ void kernel_measure(fun_ob & f,ostream & os,compress_option comp_option,block<do
 
 /* this function allows us to capture compress and non-compress runs using kernel_measure and the different levels of compute complexity */
 template <typename allocator_type>
-void option_coordinator (double coef,double step,double y_initial, double t_initial,double t_limit) {
-    ofstream out ("kernel_measure.log");
+void option_coordinator(ostream & out, double coef,double step,double y_initial, double t_initial,double t_limit) {
     Block_selector<(size_type) ARRAY_SIZE> bs;
     block<double,allocator_type> block_array[ARRAY_SIZE];
     init_array(block_array);
@@ -164,21 +170,29 @@ void option_coordinator (double coef,double step,double y_initial, double t_init
     int pos,count = 0;
     /* continue to select blocks in positions, until 10% of the array has been used. atwhich point the -1 is returned */
     while((pos = bs()) != -1) {
-        os << " position is : " << pos << " percentage done: " << count*100/ARRAY_SIZE << "%"; 
+        out << " position is : " << pos<< std::endl; 
         size_type cols = block_array[pos].dim0();
-        block<double,allocator_type> * block_ptr = & block_array[pos], * end = block_ptr + cols;
+        double * block_ptr = block_array[pos].begin(), * end = block_ptr + cols;
         /* generate the initial computation states for functions */
-        level1_compute f_lvl1(block_ptr,coef,step,cols,end);
-        level3_compute f_lvl3(y_initial,t_initial,step,t_limit);
-        
-        for (compress_option comp_option : compress_option.values()) {
-             kernel_measure(f_lvl1,out,comp_option,block_array[pos]);
-             kernel_measure(f_lvl3,out,comp_option,block_array[pos]);
-        }
+        level1_compute<allocator_type> f_lvl1(block_ptr,coef,end);
+        level2_compute<allocator_type> f_lvl2(block_ptr,coef,step,cols,end);
+        level3_compute<allocator_type> f_lvl3(y_initial,t_initial,step,t_limit);
+        kernel_measure(f_lvl1,out,block_array[pos]);
+        kernel_measure(f_lvl2,out,block_array[pos]);
+        kernel_measure(f_lvl3,out,block_array[pos]);
     }
 }
 
 int main () {
-    //2.0 coef,1.0 step,0.0 y_initial, 0.0 t_initial,1000 t_limit
-    option_coordinator<neuromapp::cstandard>(2.0,1.0,0.0,0.0,1000.0);
+    ofstream out("kernel_measure.log");
+    double t_limit = 1000.0;
+    double count = 0;
+    for ( double y_param = 0.0 ; y_param < 10.0; y_param += 1.0) {
+        for (double step = 0.001;step < 10; step *= 10) {
+            std::cout << "progress: " <<setprecision(3) << 100*count++/(10*4) << "%" << std::endl;
+            out << "y_param " << y_param << " t_limit " << t_limit << " step " << step << std::endl;
+            option_coordinator<neuromapp::cstandard>(out,2.0,step,y_param,0.0,t_limit);
+        }
+    }
 }
+
