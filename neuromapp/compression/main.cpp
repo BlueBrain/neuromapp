a8135a6c5d8b051c751b970c4a38d7bca03758c8
DevinBayly <baylyd@email.arizona.edu>
2017-08-05 09:59:26 +0800

    create default for compression app as benchmark on select file

--- neuromapp/compression/main.cpp
+++ neuromapp/compression/main.cpp
@@ -53,8 +53,10 @@ typedef size_t size_type;
 int comp_execute(int argc,char *const argv[])
 {
     //now the program options section
+    int exit_value=0;
     try {
         // create the timer 
+        string fname;
         Timer timer;
         //make desc
         po::options_description desc{"Allowed options"};
@@ -65,7 +67,6 @@ int comp_execute(int argc,char *const argv[])
             ("compress","perform a compress&uncompress on file specific block (prints out results)")
             ("sort","perform a sort on the block before any other operations")
             ("split","create new block of floating points split into their sign exponent mantissa repsentations")
-            ("path","")
             ("align","use non-standard allocator for block process")
             ("kernel_measure","run increasingly complex calculations on block comparing timing performance tradeoff for compression")
             ("stream_benchmark","use a McCalpin STREAM inspired set of benchmarks to measure bandwith on the computer")
@@ -81,7 +82,7 @@ int comp_execute(int argc,char *const argv[])
             std::cout << desc << std::endl;
         }
         if(vm.count("file") && ! vm.count("benchmark") && ! vm.count("kernel_measure")){
-            string fname = vm["file"].as<std::string>();
+            fname = vm["file"].as<std::string>();
             ifstream ifile(fname);
             if (vm.count("align")) {
                 block<double,neuromapp::align> b1;
@@ -92,21 +93,6 @@ int comp_execute(int argc,char *const argv[])
                 ifile >> b1;
                 file_routine(b1,vm,timer);
             }
-        }
-        if (vm.count("path")) {
-            std::cout << mapp::path_specifier::give_path() << std::endl;;
-        }
-        if(vm.count("benchmark")) {
-            /* gett more info about how to access files, and iterate over the directory of entries */
-            vector<string> fname_vect {"../compression/trans_data/values_10_a8213trans_bulk.csv"};
-            for (string fname : fname_vect) {
-                block<double,neuromapp::cstandard> std_block;
-                block<double,neuromapp::align> align_block;
-                /* now pass both versions through the file routine separately */
-                file_routine(std_block,vm,timer);
-                file_routine(align_block,vm,timer);
-            }
-
         }
 
         if (vm.count("stream_benchmark")) {
@@ -131,11 +117,30 @@ int comp_execute(int argc,char *const argv[])
             }
         }
 
+        //run benchmark as a default on file given
+        else {
+            if (fname.empty()) fname = mapp::path_specifier::give_path()+ "block_data/values_10_a8213trans_bulk.csv";
+            ifstream ifile;
+            ifile.open(fname);
+            block<double,neuromapp::cstandard> std_block;
+            ifile>> std_block;
+            ifile.close();
+            ifile.clear();
+            ifile.open(fname);
+            block<double,neuromapp::align> align_block;
+            ifile.close();
+            /* now pass both versions through the file routine separately */
+            std::cout << "-- cstandard benchmark routine -- " << std::endl;
+            bench_routine(std_block,timer);
+            std::cout << "-- align benchmark routine -- " << std::endl;
+            bench_routine(align_block,timer);
+        }
 
     } catch (po::error &e) {
         std::cerr << e.what()  << std::endl;
+        exit_value = 1;
     }
     // not quite sure what the return should be in this scenario
-    return 0;
+    return exit_value;
 }
 
--- neuromapp/compression/main_functions.h
+++ neuromapp/compression/main_functions.h
@@ -36,51 +36,51 @@ using neuromapp::stream_bench;
 typedef size_t size_type;
 
 template<typename allocator_type>
-            /**
-            * k_m_routine 
-            *
-            *
-            * @brief
-            *
-            * @param string & fname
-            *
-            * @return void
-            */
+/**
+ * k_m_routine 
+ *
+ *
+ * @brief
+ *
+ * @param string & fname
+ *
+ * @return void
+ */
 void k_m_routine(string & fname) {
     neuromapp::run_km<allocator_type>(fname);
 }
 
 template <typename allocator_type>
-            /**
-            * sort_routine 
-            *
-            *
-            * @brief
-            *
-            * @param  block<double,allocator_type> & block,ostream & os, Timer & time_it
-            *
-            * @return void
-            */
-void sort_routine ( block<double,allocator_type> & block,ostream & os, Timer & time_it) {
+/**
+ * sort_routine 
+ *
+ *
+ * @brief
+ *
+ * @param  block<double,allocator_type> & block, Timer & time_it
+ *
+ * @return void
+ */
+void sort_routine ( block<double,allocator_type> & block, Timer & time_it) {
     time_it.start();
 
     size_type sorting_row = 0;
     neuromapp::col_sort(&block,sorting_row);
     time_it.end();
-    os << "sorting took " << time_it.duration () << " ms " << std::endl;
+    std::cout << "sorting took " << time_it.duration () << " ms " << std::endl;
 }
 
 template <typename value_type,typename allocator_type>
-            /**
-            * stream_bench_routine 
-            *
-            *
-            * @brief
-            *
-            * @param 
-            *
-            * @return void
-            */
+/**
+ * stream_bench_routine 
+ *
+ *
+ * @brief
+ *
+ * @param 
+ *
+ * @return void
+ */
 void stream_bench_routine() {
     //create two different stream_bench objects, one compress one not
     stream_bench<value_type,allocator_type> non_str_bench(false);
@@ -92,77 +92,91 @@ void stream_bench_routine() {
 }
 
 template <typename value_type,typename allocator_type>
-            /**
-            * compress_routine 
-            *
-            *
-            * @brief
-            *
-            * @param  block<value_type,allocator_type> & block,ostream & os, Timer & time_it
-            *
-            * @return void
-            */
-void compress_routine ( block<value_type,allocator_type> & block,ostream & os, Timer & time_it) {
-        time_it.start();
-        block.compress();
-        time_it.end();
-        //compress into miliseconds
-        os << " compressed memory size: " <<  block.get_current_size() 
-            << " starting memory size: " <<  block.memory_allocated()
-            << " compression speed: " <<time_it.duration();
-        time_it.start();
-        block.uncompress();
-        time_it.end();
-        os << " uncompressed memory size: " <<  block.get_current_size() 
-            << " starting memory size: " <<  block.memory_allocated()
-            << " uncompression speed: " << time_it.duration();
+/**
+ * compress_routine 
+ *
+ *
+ * @brief
+ *
+ * @param  block<value_type,allocator_type> & block, Timer & time_it
+ *
+ * @return void
+ */
+void compress_routine ( block<value_type,allocator_type> & block, Timer & time_it) {
+    time_it.start();
+    block.compress();
+    time_it.end();
+    //compress into miliseconds
+    std::cout << " compressed memory size: " <<  block.get_current_size() 
+        << " starting memory size: " <<  block.memory_allocated()
+        << " compression speed: " <<time_it.duration() << std::endl;
+    time_it.start();
+    block.uncompress();
+    time_it.end();
+    std::cout << " uncompressed memory size: " <<  block.get_current_size() 
+        << " starting memory size: " <<  block.memory_allocated()
+        << " uncompression speed: " << time_it.duration() << std::endl;
 }
 
 /* so the routines can have a block that is either cstandard or align */
 template <typename allocator_type>
-            /**
-            * split_routine 
-            *
-            *
-            * @brief
-            *
-            * @param  block<double,allocator_type> & unsplit_block,ostream & os, Timer & time_it
-            *
-            * @return void
-            */
-void split_routine ( block<double,allocator_type> & unsplit_block,ostream & os, Timer & time_it) {
-        time_it.start();
-        block<unsigned int, allocator_type> split_block = neuromapp::generate_split_block(unsplit_block);
-        time_it.end();
-        os << "splitting took " << time_it.duration() << " ms" << std::endl;
-        compress_routine<unsigned int,allocator_type>(split_block,os,time_it);
+/**
+ * split_routine 
+ *
+ *
+ * @brief
+ *
+ * @param  block<double,allocator_type> & unsplit_block, Timer & time_it
+ *
+ * @return void
+ */
+void split_routine ( block<double,allocator_type> & unsplit_block, Timer & time_it) {
+    time_it.start();
+    block<unsigned int, allocator_type> split_block = neuromapp::generate_split_block(unsplit_block);
+    time_it.end();
+    std::cout << "splitting took " << time_it.duration() << " ms" << std::endl;
+    compress_routine<unsigned int,allocator_type>(split_block,time_it);
 }
 
 template <typename allocator_type>
-            /**
-            * file_routine 
-            *
-            *
-            * @brief
-            *
-            * @param  block<double,allocator_type> & b1,po::variables_map & vm,Timer & time_it
-            *
-            * @return void
-            */
+/**
+ * file_routine 
+ *
+ *
+ * @brief
+ *
+ * @param  block<double,allocator_type> & b1,po::variables_map & vm,Timer & time_it
+ *
+ * @return void
+ */
 void file_routine( block<double,allocator_type> & b1,po::variables_map & vm,Timer & time_it) {
-    ofstream out("compress_app_run.log");
     if(vm.count("split")) {
-        split_routine(b1,out,time_it);
+        split_routine(b1,time_it);
         return;// the split block doesn't get returned so don't proceed even if the options are given
     } 
     if ( vm.count("sort") ) {
-        sort_routine(b1,out,time_it);
+        sort_routine(b1,time_it);
     }
     if ( vm.count("compress") ) {
-        compress_routine(b1,out,time_it);
+        compress_routine(b1,time_it);
     }
 }
 
-
+template <typename allocator_type>
+/**
+ * bench_routine 
+ *
+ *
+ * @brief
+ *
+ * @param  block<double,allocator_type> & b1,Timer & time_it
+ *
+ * @return void
+ */
+void bench_routine( block<double,allocator_type> & b1,Timer & time_it) {
+    sort_routine(b1,time_it);
+    compress_routine(b1,time_it);
+    split_routine(b1,time_it);
+}
 
 #endif
